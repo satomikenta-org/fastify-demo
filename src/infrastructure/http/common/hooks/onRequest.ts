10f5ff5861028a5f2f6d31c08fb84f531f7a5318
satomi kenta <satomikennta1213@gmail.com>
2021-07-21 13:20:45 +0900

    modify: prettier-printWidth

--- src/infrastructure/http/common/hooks/onRequest.ts
+++ src/infrastructure/http/common/hooks/onRequest.ts
@@ -1,27 +1,15 @@
-import {
-  FastifyInstance,
-  FastifyReply,
-  FastifyRequest,
-  HookHandlerDoneFunction,
-} from "fastify";
+import { FastifyInstance, FastifyReply, FastifyRequest, HookHandlerDoneFunction } from 'fastify';
 
 export function setOnRequestHook(server: FastifyInstance) {
-  server.addHook(
-    "onRequest",
-    (
-      request: FastifyRequest,
-      _: FastifyReply,
-      done: HookHandlerDoneFunction
-    ) => {
-      request.log.info(
-        {
-          url: request.raw.url,
-          method: request.method,
-          ip: request.ip,
-        },
-        "request"
-      );
-      done();
-    }
-  );
+  server.addHook('onRequest', (request: FastifyRequest, _: FastifyReply, done: HookHandlerDoneFunction) => {
+    request.log.info(
+      {
+        url: request.raw.url,
+        method: request.method,
+        ip: request.ip,
+      },
+      'request'
+    );
+    done();
+  });
 }
--- src/infrastructure/http/common/setCommonHandlers.ts
+++ src/infrastructure/http/common/setCommonHandlers.ts
@@ -1,33 +1,26 @@
-import {
-  FastifyError,
-  FastifyInstance,
-  FastifyReply,
-  FastifyRequest,
-} from "fastify";
+import { FastifyError, FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 
 export function setCommonHandlers(server: FastifyInstance) {
-  server.setErrorHandler(
-    (error: FastifyError, request: FastifyRequest, reply: FastifyReply) => {
-      let { statusCode, message } = error;
+  server.setErrorHandler((error: FastifyError, request: FastifyRequest, reply: FastifyReply) => {
+    let { statusCode, message } = error;
 
-      if (!statusCode) statusCode = 500;
+    if (!statusCode) statusCode = 500;
 
-      if (statusCode < 500) {
-        request.log.warn(message);
-      } else {
-        request.log.error(message);
-      }
-
-      reply.status(statusCode).send({
-        statusCode: statusCode,
-        error: true,
-        message,
-        requestId: request.id,
-      });
+    if (statusCode < 500) {
+      request.log.warn(message);
+    } else {
+      request.log.error(message);
     }
-  );
+
+    reply.status(statusCode).send({
+      statusCode: statusCode,
+      error: true,
+      message,
+      requestId: request.id,
+    });
+  });
 
   server.setNotFoundHandler((_: FastifyRequest, reply: FastifyReply) => {
-    reply.status(404).send("Not Found");
+    reply.status(404).send('Not Found');
   });
 }
